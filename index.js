870589291be3bc2545b086ab3791c9ac73ec08c8
CarlEverest78 <thepythoneer@gmail.com>
2022-05-17 12:34:52 +0200

    last commit

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-// Grab the emojiContainer div and store it in a variable. Log it to the console
+let myEmojis = ["😊","👨‍👩‍👧","🚵‍♂️","🏖️","🔥","🏃‍♂️","👨‍💻","🧑‍💻"]
+const emojiContainer = document.getElementById("emoji-container")
+const emojiInput = document.getElementById("emoji-input")
+const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
 
-const myEmojis = ["👨‍💻", "⛷", "🍲"]
-
-const emojiContainer = document.getElementById("emojiContainer")
-console.log(emojiContainer)
-
-for (let i = 0; i < myEmojis.length; i++) {
-    // console.log(myEmojis[i])
+function renderEmojis() {
+    emojiContainer.innerHTML = ""
+    for (let i = 0; i < myEmojis.length; i++) {
+        let spanEl = document.createElement("span")
+        spanEl.textContent = myEmojis[i]
+        emojiContainer.append(spanEl)
+        // emojiContainer.innerHTML += `<span>${myEmojis[i]}</span>`
+        emojiInput.value = ""   
+    }
 }
+
+renderEmojis()
+
+pushBtn.addEventListener("click", function() {
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        renderEmojis()        
+    }
+})
+
+unshiftBtn.addEventListener("click", function() {
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        renderEmojis()
+    }
+})
+
+popBtn.addEventListener("click", function() {
+    myEmojis.pop()
+    renderEmojis()
+})
+
+shiftBtn.addEventListener("click", function() {
+    myEmojis.shift()
+    renderEmojis()
+})
