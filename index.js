13231459c8790325e2778c1a5bf7ccf9ef2068c3
CarlEverest78 <thepythoneer@gmail.com>
2022-05-17 11:06:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Grab the emojiContainer div and store it in a variable. Log it to the console
+
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+
+const emojiContainer = document.getElementById("emojiContainer")
+console.log(emojiContainer)
+
+for (let i = 0; i < myEmojis.length; i++) {
+    // console.log(myEmojis[i])
+}
